544e25429cd1ff17387d2d756def72a8454ecec0
Kenna236 <ndudimkenna@gmail.com>
2023-06-20 09:03:20 -0700

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: 'Integer to be added'
+ * @b: 'Integer to be added'
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
